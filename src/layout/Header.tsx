66940564700090a575a4e205e2291339a60e2b4f
thanhnhat <nhatnt1@tinhvan.com>
2023-08-14 00:40:48 +0700

    add tailwind, typescript

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -5,7 +5,9 @@ import CountMessage from "../components/shared/CountMessage";
 export default function Header() {
     return (
         <div className={`bg-header flex fixed top-0 w-full`} style={{height: "64px"}}>
-          <div className={`w-[224px]`}></div>
+          <div className={`w-[224px] py-[10px] px-4 cursor-pointer`}>
+            <img width={141} alt={''} src={'/chat/logo.png'}/>
+          </div>
           <NavItem preIcon={<CountMessage total={2}/>} title={"Hội thoại"}/>
           <NavItem preIcon={<IconAnalyst/>} title={"Thống kê"}/>
           <NavItem preIcon={<IconSetting/>} title={"Cài đặt"}/>
