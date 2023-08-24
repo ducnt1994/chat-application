ac354999737374fdd7788bfdadfdf06a940d2bf8
trungduc <ducnt@ecomobi.com>
2023-08-24 21:24:55 +0700

    get token data cookie

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,7 @@
 import {Filter} from "../components/home/Filter";
 import Conversation from "../components/home/Conversation";
-import Cookies from "js-cookie";
 
 export default function Home() {
-  const data = Cookies.get('accessToken');
-  console.log(data)
   return (
     <div className={`flex max-h-max-screen h-max-screen`}>
       <Filter />
